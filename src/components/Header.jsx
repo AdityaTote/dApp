e06f7b9a1431107b145e308b73952c7fdb3f3aa1
AdityaTote <adityatote24@gmail.com>
2024-10-01 02:45:17 +0530

    chnaged header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export function Header() {
     return (
       <nav className="bg-gray-800 p-3 shadow-md">
         <div className="container mx-auto flex items-center justify-between">
-          <h1 className="text-white text-2xl font-bold">DApp</h1>
+          <h1 className="text-white text-2xl font-bold">Wallet Adapter</h1>
         </div>
       </nav>
     );
